0508339115d3738f01e80bb13732b7ce548077b1
dldyou <chankyu2003@gmail.com>
2024-04-09 22:58:19 +0900

    🎉 add: backend (NodeJS)

---
+++ backend/routes/account.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/login', (req, res) => {
+
+});
+
+router.get('/logout', (req, res) => {
+
+});
+
+router.post('/add', (req, res) => {
+
+});
+
+router.post('/delete', (req, res) => {
+
+});
+
+router.get('/list', (req, res) => {
+
+});
+
+router.post('/reset', (req, res) => {
+
+});
+
+router.get('/log', (req, res) => {
+
+});
+
+module.exports = router;
---
+++ backend/routes/file.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+
+router.get('/upload', (req, res) => {
+
+});
+
+router.get('/download', (req, res) => {
+
+});
+
+router.get('/search', (req, res) => {
+
+});
+
+// Get the header of the CSV file (TEST)
+router.get('/get-csv-header/:hospital', (req, res) => {
+    var FILE_NAME = `${req.params.hospital}.csv`;
+    var filePath = path.join(__dirname, '..', 'public', 'csv', FILE_NAME);
+    var file = fs.readFileSync(filePath, 'utf-8');
+    var lines = file.split('\r\n' || '\n' || '\r');
+    var header = lines[0].split(', ').map((item) => item.replace(/"/g, ''));
+    res.json({ header: header });
+});
+
+module.exports = router;
