6e9cbb6f6496cf3ca889021ad0ccaef94c598c66
jhmin99 <mjh012399@gmail.com>
2024-05-04 22:49:53 +0900

    ✨ feat: add jwt middleware

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,8 @@ const morgan = require("morgan");
 const { logs } = require("./config/vars");
 const mongoose = require("./config/mongoose");
 const logger = require("./config/logger");
+//jwt
+const jwt = require('jsonwebtoken');
 
 // MongoDB 연결
 mongoose.connect();
@@ -33,6 +35,24 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "uploads")));
 
+// JWT 검증 미들웨어
+app.use(function (req, res, next) {
+    if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+            if (err) {
+                req.user = undefined; // 인증 실패
+                return res.status(401).json({ message: 'Invalid token' });
+            }
+            req.user = decoded; // 인증 성공, 사용자 정보 저장
+            next();
+        });
+    } else {
+        req.user = undefined; // 토큰 없음
+        next();
+    }
+});
+
 // 라우트 핸들러 설정
 app.use("/account", accountRouter);
 app.use("/file", fileRouter);
@@ -44,17 +64,18 @@ app.use(function (req, res, next) {
 
 // 기본 에러 핸들링 미들웨어
 app.use(function (err, req, res, next) {
-    // 개발 환경에서는 에러 정보를 로컬 변수에 저장
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "dev" ? err : {};
-
     // 모든 환경에서 에러 로그를 기록
     logger.error(`Status: ${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
 
-    // 에러 페이지를 클라이언트에 응답
-    res.status(err.status || 500);
-    res.render("error");
+    // 에러 정보를 JSON 형식으로 클라이언트에 응답
+    res.status(err.status || 500).json({
+        message: err.message,
+        error: req.app.get("env") === "dev" ? err : {}
+    });
 });
+
+
+
 
 // 모듈 외부로 app 객체 공개
 module.exports = app;
