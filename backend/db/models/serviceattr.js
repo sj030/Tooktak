02b38bcef72867ea24e64c4f566c7e69e27b403a
dldyou <chankyu2003@gmail.com>
2024-05-05 00:14:55 +0900

    ✨ Feat: 서비스 추가 / 서비스 이름 반환 / 서비스 속성 반환 함수 추가

--- backend/db/models/serviceattr.js
+++
@@ -1,9 +0,0 @@
-const { ServiceAttrModel } = require("../schemas/serviceattr");
-
-class ServiceAttrRepository {
-    static async addServiceAttr(serviceAttr) {
-
-    }
-}
-
-module.exports = { ServiceAttrRepository };
