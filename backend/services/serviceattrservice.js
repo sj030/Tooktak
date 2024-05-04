deee8da2c483da41f2ee403c6b588e368bfe667b
dldyou <chankyu2003@gmail.com>
2024-05-05 00:18:10 +0900

    ✨ Feat: ServiceAttrRepository에 대한 서비스단 추가

--- backend/services/serviceattrservice.js
+++ backend/services/serviceattrservice.js
@@ -1,7 +1,27 @@
-const { ServiceAttrRepository } = require("../db/models/serviceattr");
+const { ServiceAttrRepository } = require("../db/models/serviceattrs");
 
 class ServiceAttrService {
-    static async addServiceAttr() {
+    /**
+     * 서비스를 추가합니다.
+     * @param {*} attributes 
+     */
+    static async addService(attributes) {
+        await ServiceAttrRepository.addService(attributes);
+    }
+
+    /**
+     * @returns {Array} 모든 서비스 이름을 가져옵니다.
+     */
+    static async getAllServicesName() {
+        return await ServiceAttrRepository.getAllServicesName();
+    }
+
+    /**
+     * @param {String} name 
+     * @returns {*} 이름이 `name`인 서비스의 속성을 가져옵니다.
+     */
+    static async getServiceByName(name) {
+        return await ServiceAttrRepository.getServiceByName(name);
     }
 }
 
