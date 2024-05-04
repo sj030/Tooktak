164eb2f58f9fa15a9cf6dda0e72a86f528f9da0a
jhmin99 <mjh012399@gmail.com>
2024-05-04 23:29:44 +0900

    ✨ feat: add Selective application of authentication token middleware

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,29 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const {authService} = require("../services/authservice");
+const authenticateToken = require('../middleware/authenticateToken'); // Import the middleware
+
 router.post("/login", authService.login);
 
-router.get("/logout", (req, res) => {
+router.get("/logout", authenticateToken, (req, res) => {
 
 });
 
-router.post("/add", (req, res) => {
+router.post("/add", authenticateToken, (req, res) => {
 
 });
 
-router.post("/delete", (req, res) => {
+router.post("/delete", authenticateToken, (req, res) => {
 
 });
 
-router.get("/list", (req, res) => {
+router.get("/list", authenticateToken, (req, res) => {
 
 });
 
-router.post("/reset", (req, res) => {
+router.post("/reset", authenticateToken, (req, res) => {
 
 });
 
-router.get("/log", (req, res) => {
+router.get("/log", authenticateToken, (req, res) => {
 
 });
 
