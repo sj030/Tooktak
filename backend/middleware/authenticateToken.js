d4da47beaed09d997377d3d406c8f98ac09ba03b
jhmin99 <mjh012399@gmail.com>
2024-05-05 02:49:58 +0900

    🐛 fix: jwt verify수정

--- backend/middleware/authenticateToken.js
+++ backend/middleware/authenticateToken.js
@@ -1,20 +1,29 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const { UserRepository } = require("../db/models/users");
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) {
+    if (!token) {
         return res.sendStatus(401); // No token provided
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
         if (err) {
-            return res.status(401).json({ message: 'Invalid token' });
+            return res.status(401).json({ message: `Invalid token: ${err.message}` });        
         }
 
-        req.user = decoded;
-        next();
+        try {
+            const user = await UserRepository.findById(decoded.id);
+            if (!user) {
+                return res.status(401).json({ message: "Authentication Error" });
+            }
+            req.user = user; // Save the user to req.user
+            next();
+        } catch (error) {
+            return res.status(500).json({ message: "Server Error" });
+        }
     });
 }
 
