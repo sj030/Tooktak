0ac891f63c71308a0ceba6cfb135810fd511dd23
CHANGYU <window9u@konkuk.ac.kr>
2024-05-10 17:14:45 +0900

    🌱 seed: new page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -5,16 +5,24 @@ import {Button} from "../components/commons/Button";
 export default function Admin() {
   return (
     <LargeSection>
-      <Card header={"사용자 계정"} footer={<Button children={"계정 삭제"} color={"red"}/>}>
-        <Table
-          header={["이름", "id", "소속"]}
-          items={[
-            ["문찬규", "정상", "개발자"],
-            ["김민수", "정상", "분석가"],
-            ["이상민", "정상", "분석가"],
-          ]}
-        />
-      </Card>
+      <Card header={"사용자 계정"}
+            body={
+                <Table
+                    header={["이름", "id","pw", "소속"]}
+                    items={[
+                        ["문찬규", "djawnstlr1422","ds", "개발자"],
+                        ["김민수", "minsu","1234", "분석가"],
+                        ["엄준식", "junsic123","1234", "개발자"],
+                    ]}
+                />
+            }
+            footer={
+          <>
+              <Button children={"계정 추가"} color={"green"}/>
+          <Button children={"계정 삭제"} color={"red"}/>
+      </>}
+      />
+
     </LargeSection>
   );
 }
