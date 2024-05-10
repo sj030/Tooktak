70a4fa9c7377e593889ee4c71865275cde32ea60
CHANGYU <window9u@konkuk.ac.kr>
2024-05-10 17:01:05 +0900

    ✨ Feat: table common component

--- src/components/commons/TableRow.jsx
+++ src/components/commons/TableRow.jsx
@@ -1,11 +1,12 @@
 import {useState} from "react";
 
-export function TableRow({item, onClick}) {
+export function TableRow({item, onClick=null}) {
     const [selected, setSelected] = useState(false);
     return (
         <tr
             onClick={() => {
                 setSelected(!selected)
+                onClick && onClick()
             }}
             className={selected ? "is-selected" : ""}>
             {item.map((d) => (
