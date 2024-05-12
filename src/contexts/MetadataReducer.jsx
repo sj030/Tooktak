b4e94d19bdf446a6ffe0e3903f69db1d88c31b67
CHANGYU <window9u@konkuk.ac.kr>
2024-05-13 08:50:50 +0900

    ♻️ Refactor: reducer map function

--- src/contexts/MetadataReducer.jsx
+++ src/contexts/MetadataReducer.jsx
@@ -6,34 +6,26 @@ export function metadataReducer(state, action) {
             return {hospital: action.hospital, attributes: action.attributes};
         case "TEXT_ATTRIBUTE":
             return {
-                ...state, attributes: state.attributes.map((attribute) => {
-                    if (attribute.name === action.name) {
-                        return {...attribute, value: action.value}
-                    }
-                    return attribute;
-                })
+                ...state, attributes: state.attributes.map((attribute) =>
+                    attribute.name === action.name ? {...attribute, value: action.value} : attribute
+                )
             }
         case "RANGE_ATTRIBUTE":
             return {
-                ...state, attributes: state.attributes.map((attribute) => {
-                    if (attribute.name === action.name) {
-                        return {...attribute, start: action.start, end: action.end}
-                    }
-                    return attribute;
-                })
+                ...state, attributes: state.attributes.map((attribute) =>
+                    attribute.name === action.name ? {...attribute, start: action.start, end: action.end} : attribute
+                )
             }
         case "CHECKBOX_ATTRIBUTE":
             return {
-                ...state, attributes: state.attributes.map((attribute) => {
-                    if (attribute.name === action.name) {
-                        return {...attribute, value: action.value}
-                    }
-                    return attribute;
-                })
+                ...state, attributes: state.attributes.map((attribute) =>
+                    attribute.name === action.name ? {...attribute, value: action.value} : attribute
+                )
             }
         case "RESET_ATTRIBUTE":
-            return {...state, attributes:state.attributes.map((attribute) => {
-                return {...attribute, value: "",start: "", end: ""}
+            return {
+                ...state, attributes: state.attributes.map((attribute) => {
+                    return {...attribute, value: "", start: "", end: ""}
                 })
             }
         default:
