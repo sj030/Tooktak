e8ccff487b038d22411d721055f8cc5cf2585d6a
CHANGYU <window9u@konkuk.ac.kr>
2024-05-13 11:13:16 +0900

    ♻️ Refactor: make invisible when there is no files

--- src/components/service/search/file/FileTable.jsx
+++ src/components/service/search/file/FileTable.jsx
@@ -1,18 +1,17 @@
 import {Table} from "../../../commons/Table";
-import {
-    useFileAttribute,
-    useFileList,
-    useSelectFile,
-} from "../../../../contexts/FileContext";
+import {useFileAttribute, useFileList, useSelectFile,} from "../../../../contexts/FileContext";
 import {Button} from "../../../commons/Button";
 
 export function FileTable() {
     const items = useFileList();
     const attributes = useFileAttribute();
-    const {select,selectAll,unselectAll} = useSelectFile();
+    const {select, selectAll, unselectAll} = useSelectFile();
     return <>
-        <Button children={"모두 선택"} onClick={selectAll} color={"blue"}/>
-        <Button children={"모두 해제"} onClick={unselectAll} color={"red"}/>
+        {items.length > 0 ? <>
+            <Button children={"모두 선택"} onClick={selectAll} color={"blue"}/>
+            <Button children={"모두 해제"} onClick={unselectAll} color={"red"}/>
+        </> : null
+        }
         <Table header={attributes}
                items={items}
                select={select}
