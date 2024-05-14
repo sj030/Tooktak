0144d20cd5d42b64c615c61f784b3f6e74f8ffcd
jhmin99 <mjh012399@gmail.com>
2024-05-14 10:44:38 +0900

    ✨ Feat: onClick event handler with auth

--- src/components/service/Navigation.jsx
+++ src/components/service/Navigation.jsx
@@ -1,22 +1,71 @@
 import { Menu, MenuLabel, MenuList, MenuItem } from "../commons/Menu";
+import Modal from "../commons/Modal";
 import logo from "../../asset/logo.png";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext , useState} from "react";
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from "../../services/config/axiosInstance";
+
 export default function Navigation() {
+  const { isLoggedin, logout } = useContext(AuthContext);
+  const navigate = useNavigate(); // useNavigate 훅 사용
+  const [showModal, setShowModal] = useState(false);
+
+  const handleNavigation = (path) => {
+    if (!isLoggedin) {
+      navigate('/login');
+    } else {
+      navigate(path);
+    }
+  };
+
+  const handleNavigationAdmin = async (path) => {
+    if (!isLoggedin) {
+      navigate('/login');
+    } else {
+      try {
+        const response = await axiosInstance.get("/account/admin"); // GET 요청을 가정
+        if (response.status === 403) {
+          setShowModal(true); // 권한 없음 모달 표시
+        } else {
+          navigate(path); // 정상적으로 페이지 이동
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 403) {
+          setShowModal(true); // 서버에서 403 응답을 반환한 경우
+        } else {
+          console.error('Error accessing the page:', error);
+        }
+      }
+    }
+  };
+
+  const onLogout = () => {
+    logout();
+    navigate('/login');
+  };
+
   return (
     <Menu logo={logo}>
       <MenuLabel label={"계정"} />
       <MenuList>
-        <MenuItem path={"/login"} name={"로그인"} />
+        {isLoggedin ? (
+          <MenuItem path="/" name="로그아웃" onClick={onLogout} />
+        ) : (
+          <MenuItem name="로그인" onClick={() => handleNavigation('/login')}/>
+        )}
       </MenuList>
       <MenuLabel label={"데이터 관리"} />
       <MenuList>
-        <MenuItem path={"/search"} name={"데이터 검색"} />
-        <MenuItem path={"/upload"} name={"데이터 업로드"} />
+        <MenuItem name={"데이터 검색"} onClick={() => handleNavigation('/search')} />
+        <MenuItem name={"데이터 업로드"} onClick={() => handleNavigation('/upload')} />
       </MenuList>
       <MenuLabel label={"관리자 메뉴"} />
       <MenuList>
-          <MenuItem path={"/admin"} name={"계정 관리"} />
-          <MenuItem path={"/log"} name={"로그 확인"} />
+        <MenuItem name={"계정 관리"} onClick={() => handleNavigationAdmin('/admin')}/>
+        <MenuItem name={"로그 확인"} onClick={() => handleNavigationAdmin('/log')}/>
       </MenuList>
+      {showModal && <Modal onClose={() => setShowModal(false)}>You do not have permission to access this.</Modal>}
     </Menu>
   );
 }
