7bdd244644f6c209ededf58270fb7d4f5669302f
dldyou <chankyu2003@gmail.com>
2024-05-15 20:02:09 +0900

    ✨ Feat: 파일 검색 관련 함수 추가

--- backend/services/fileservice.js
+++ backend/services/fileservice.js
@@ -1,8 +1,6 @@
 const { FileRepository } = require("../db/models/files");
-const JSZip = require("jszip");
-const path = require("path");
-const fs = require("fs");
-const ftp = require("basic-ftp")
+
+const standardFileAttrs = ["f_name", "f_extension", "f_path", "f_date", "chapter", "p_no"];
 
 /**
  * option에 따라 subQuery를 생성합니다.
@@ -27,9 +25,14 @@ function getSubQuery(option, query) {
 }
 
 function makeQueryWithAttr(serviceAttrs, query, key) {
-    const serviceAttr = serviceAttrs.find(serviceAttr => serviceAttr.name === key);
-    if (serviceAttr) {
-        return getSubQuery(serviceAttr.option, query[key]);
+    let attr = null;
+    for (attr of serviceAttrs) {
+        if (attr.name === key) {
+            break;
+        }
+    }
+    if (attr) {
+        return getSubQuery(attr.option, query);
     }
     return {};
 }
@@ -38,14 +41,15 @@ class FileService {
     /**
      * 파일을 검색합니다.
      * @param {*} serviceAttrs - 서비스의 속성 배열입니다. 
-     * `{ name: String, attributes: [ { name: String, option: String, type: String, list: [ Mixed ] } ] }` 에서 `attributes`에 해당하는 배열입니다.
+     * `{ serviceName: String, attributes: [ { name: String, option: String, type: String, list: [ Mixed ] } ] }` 형식입니다.
      * @param {*} query - `{ name: { text: String } }` 또는 `{ name: { min: Number | Date, max: Number | Date } }` 또는 `{ name: { list: [ Mixed ] }`의 배열입니다.
      * @returns {Array} query에 해당하는 파일들을 반환합니다.
      */
     static async getAllMetaDataByQuery(serviceAttrs, query) {
         let searchQuery = {};
         for (const key in query) {
-            searchQuery[key] = makeQueryWithAttr(serviceAttrs, query[key], key);
+            const searchKey = standardFileAttrs.includes(key) ? key : "patient.attributes." + key;
+            searchQuery[searchKey] = makeQueryWithAttr(serviceAttrs["attributes"], query[key], key);
         }
         return await FileRepository.findAllByQuery(searchQuery);
     }
