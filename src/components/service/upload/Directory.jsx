a86742bf5426fd554165c3c525eb54018a291cf0
CHANGYU <window9u@konkuk.ac.kr>
2024-05-26 10:43:54 +0900

    ✨ Feat: upload success 일떄, file list 숨기기

--- src/components/service/upload/Directory.jsx
+++ src/components/service/upload/Directory.jsx
@@ -15,15 +15,16 @@ export function Directory({directory, path}) {
         <div className="message-header">
             <p>{path}</p>
         </div>
-        <div className="message-body">
-            {Object.keys(directory.files).length > 0 ? Object.entries(directory.files).map(([key, value]) =>
-                <div key={key} className="box">
-                    <p>{directory.state === "fileOnly" ? key + value.name : value["name"] ? value.name : value}</p>
-                </div>
-            ) : <div className="box">xlsx data only</div>}
-            {active && directory.state !== "fileOnly" &&
-                <div
-                    className="box">{Object.entries(directory.attributes).map(([key, val]) => key + ": " + val).join(", ")}</div>}
-        </div>
+        {directory.state !== "success" &&
+            <div className="message-body">
+                {Object.keys(directory.files).length > 0 ? Object.entries(directory.files).map(([key, value]) =>
+                    <div key={key} className="box">
+                        <p>{directory.state === "fileOnly" ? key + value.name : value["name"] ? value.name : value}</p>
+                    </div>
+                ) : <div className="box">xlsx data only</div>}
+                {active && directory.state !== "fileOnly" &&
+                    <div
+                        className="box">{Object.entries(directory.attributes).map(([key, val]) => key + ": " + val).join(", ")}</div>}
+            </div>}
     </div>
 }
