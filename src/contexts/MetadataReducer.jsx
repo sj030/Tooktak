979be0faf359c6f0734b6c6511f33598c0528e3f
CHANGYU <window9u@konkuk.ac.kr>
2024-05-27 17:29:55 +0900

    ✨ Feat: Hospital attribute checkBox 추가

--- src/contexts/MetadataReducer.jsx
+++ src/contexts/MetadataReducer.jsx
@@ -1,12 +1,14 @@
 export function metadataReducer(state, action) {
     switch (action.type) {
         case "INIT":
-            return {...action.payload, attributes: action.payload.attributes.map((attribute) => {
-                if(attribute.option === "range") {
-                    return {...attribute, value: {min: "", max: ""}}
-                }
-                return {...attribute, value: ""}
-            })};
+            return {
+                ...action.payload, attributes: action.payload.attributes.map((attribute) => {
+                    if (attribute.option === "range") {
+                        return {...attribute, value: {min: "", max: ""}}
+                    }
+                    return {...attribute, value: ""}
+                })
+            };
         case "SET_HOSPITAL":
             return {hospital: action.hospital, attributes: action.attributes};
         case "SET_ATTRIBUTE":
@@ -18,11 +20,17 @@ export function metadataReducer(state, action) {
         case "RESET_ATTRIBUTE":
             return {
                 ...state, attributes: state.attributes.map((attribute) => {
-                    if(attribute.option === "range") {
+                    if (attribute.option === "range") {
                         return {...attribute, value: {min: "", max: ""}}
                     }
                     return {...attribute, value: ""}
                 })
+            }
+        case "SET_ATTRIBUTE_VISIBLE":
+            return {
+                ...state, attributes: state.attributes.map((attribute) =>
+                    attribute.name === action.name ? {...attribute, visible: action.visible} : attribute
+                )
             }
         default:
             return state;
