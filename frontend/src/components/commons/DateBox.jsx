8c0f9b6f83917274c30c052d648ab69d12a6b37d
jhmin99 <mjh012399@gmail.com>
2024-05-29 03:00:00 +0900

    👔 Fix: apply삭제 search 버튼 하나로 통일

--- frontend/src/components/commons/DateBox.jsx
+++ frontend/src/components/commons/DateBox.jsx
@@ -1,25 +1,58 @@
-export default function DateBox({label, setStart, setEnd, start, end }) {
-    
-    return (<div className={"box"}>
-        <div className="level">
-            <div className="level-left">
-                <label className="label">{label}</label>
-            </div>
-            <div className="field has-addons is-primary is-outlined is-fullwidth">
-                <input className="input"
-                       type="date"
-                       value={start}
-                       onChange={(e) => {
-                           setStart(e.target.value)
-                       }}/>
-                <div className={"block"}>
-                    <p className={"is-size-4"}> ~ </p>
+
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+export default function DateBox({ label, start, end, setStart, setEnd, setDateRange }) {
+    const handleStartChange = (date) => {
+        setStart(date);
+        if (end) {
+            setDateRange(date, end);  // 변경된 startDate와 기존 endDate로 setDateRange 호출
+        } else {
+            setDateRange(date, date);  // endDate가 없는 경우 startDate만 반영
+        }
+    };
+
+    const handleEndChange = (date) => {
+        setEnd(date);
+        if (start) {
+            setDateRange(start, date);  // 기존 startDate와 변경된 endDate로 setDateRange 호출
+        }
+    };
+
+    return (
+        <div className={"box is-primary "}>
+            <div className="date-box">
+                <div className="level">
+                    <div className="level-left">
+                        <label className="label">{label}</label>
+                    </div>
+                    <div className="date-picker field has-addons is-primary is-outlined is-fullwidth
+">
+                        <DatePicker className="input"
+
+                            selected={start}
+                            onChange={handleStartChange}
+                            selectsStart
+                            startDate={start}
+                            endDate={end}
+                            placeholderText="시작 날짜"
+                        />
+                        <span> ~ </span>
+                        <DatePicker className="input"
+
+                            selected={end}
+                            onChange={handleEndChange}
+                            selectsEnd
+                            startDate={start}
+                            endDate={end}
+                            minDate={start}
+                            placeholderText="종료 날짜"
+                        />
+                    </div>
                 </div>
-                <input className="input" type="date" value={end} 
-                       onChange={(e) => {
-                           setEnd(e.target.value)
-                       }}/><p className="mx-1"/>
             </div>
         </div>
-    </div>);
+
+    );
 }
